420640b200d176c6ed5e40ed1850c67242f5653c
Prachi <prachikale136@gmail.com>
2023-06-30 22:50:28 +0530

    [Prachi] Add:count of remaining chapter using collection

---
+++ CollectionProblem/CollectionProblem/ChapterCount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionProblem
+{
+    internal class ChapterCount
+    {
+        public static int ChaptersToRead(int n, Dictionary<int, int> chapters, int k)
+        {
+            int chaptersToRead = 0;
+
+            foreach (var chapter in chapters)
+            {
+                int startPage = chapter.Key;
+                int endPage = chapter.Value;
+
+                if (startPage < k && k > endPage)
+                {
+                    chaptersToRead++;
+                }
+                else if (startPage > k)
+                {
+                    chaptersToRead++;
+                }
+            }
+
+            return chaptersToRead;
+        }
+    
+
+    }
+}
---
+++ CollectionProblem/CollectionProblem/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionProblem
+{
+    internal class Program
+    {
+        public static void Main(string[] args) 
+        {
+            int n = 4;
+            int k = 7;
+
+            Dictionary<int, int> chapters = new Dictionary<int, int>();
+
+            chapters.Add(1, 4);
+            chapters.Add(5, 7);
+            chapters.Add(9, 16);
+            chapters.Add(17, 26);
+
+            int chaptersToRead = ChapterCount.ChaptersToRead(n, chapters, k);
+            Console.WriteLine("The Number of Chapters to Read: " + chaptersToRead);
+        }
+    }
+}
